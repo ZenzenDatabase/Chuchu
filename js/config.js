bb8ef106033ff892866972fe9f63ee484a5c722f
Zdatabase <53589956+ZenzenDatabase@users.noreply.github.com>
2026-05-13 17:27:01 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@
 // ============================================================
 
 const SUPABASE_URL = 'https://bovqduczvwamwqsjcfef.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_fYkvSH0-f2CII9Oc5ow08g_0iz5g7rb';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJvdnFkdWN6dndhbXdxc2pjZmVmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2NDI4MTUsImV4cCI6MjA5NDIxODgxNX0.uh5-YFD4o7XfvdLPkUN60RhXoh3PYlSXjka9YzX8onY';
 
 // Create the client only ONCE
 // We use 'window.supabase' to ensure we are calling the library loaded from the CDN
