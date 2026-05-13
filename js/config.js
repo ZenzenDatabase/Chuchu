ebaa6ed70b3c41963eb740350a53955cce16cd64
Zdatabase <53589956+ZenzenDatabase@users.noreply.github.com>
2026-05-13 17:48:04 +0900

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,3 @@
-// ============================================================
-//  SUPABASE CONFIGURATION
-// ============================================================
-
 // 1. Use the long key starting with eyJ... from Project Settings -> API
 const SUPABASE_URL = 'https://bovqduczvwamwqsjcfef.supabase.co';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJvdnFkdWN6dndhbXdxc2pjZmVmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg2NDI4MTUsImV4cCI6MjA5NDIxODgxNX0.uh5-YFD4o7XfvdLPkUN60RhXoh3PYlSXjka9YzX8onY';
